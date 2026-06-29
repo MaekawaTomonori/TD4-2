3537cd189e67c28d2c9e73503413015f3314c1af
iiokaisami <k023g0025@g.neec.ac.jp>
2026-06-29 17:01:33 +0900

    コンポーネント作成Move中身作成途中

--- application/gameobject/component/action/enemy/charge/ChargeMoveComponent.cpp
+++ application/gameobject/component/action/enemy/charge/ChargeMoveComponent.cpp
@@ -1,1 +1,24 @@
 #include "ChargeMoveComponent.h"
+
+#include "engine/gameobject/base/GameObject.h"
+
+GameObjectComponent::ChargeMoveComponent::ChargeMoveComponent(GameObject* _player)
+	: player_(_player)
+{
+}
+
+void GameObjectComponent::ChargeMoveComponent::Update(GameObject* owner)
+{
+	// プレイヤーが存在しない場合は処理を中断
+	if (!player_)
+	{
+		return;
+	}
+
+	// プレイヤーの位置を取得
+	Vector3 playerPosition = player_->GetPosition();
+
+
+
+
+}
--- application/gameobject/component/action/enemy/charge/ChargeMoveComponent.h
+++ application/gameobject/component/action/enemy/charge/ChargeMoveComponent.h
@@ -1,4 +1,39 @@
 #pragma once
-class ChargeMoveComponent
+
+#include "engine/gameobject/component/base/IActionComponent.h"
+
+namespace GameObjectComponent
 {
-};
+
+  /// <summary>
+  /// チャージ移動コンポーネント
+  /// </summary>
+  class ChargeMoveComponent : public IActionComponent
+  {
+  public:
+
+	  // コンストラクタ
+	  ChargeMoveComponent(GameObject* _player);
+
+	  // デストラクタ
+	  ~ChargeMoveComponent() override = default;
+	  
+	  /// <summary>
+	  /// 毎フレームの更新処理
+	  /// </summary>
+	  /// <param name="owner">所有者</param>
+      void Update(GameObject* owner) override;
+
+  private:
+
+	  // プレイヤーのポインタ
+	  GameObject* player_ = nullptr; 
+
+	  // 移動速度
+	  float moveSpeed_ = 5.0f;
+
+
+  };
+
+
+} // namespace GameObjectComponent
