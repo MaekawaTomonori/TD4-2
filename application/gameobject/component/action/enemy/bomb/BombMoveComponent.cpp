91605fb9e4e74725e6dcadbc01ab80dba532f196
kitajima-hayato <k023g0057@g.neec.ac.jp>
2026-06-29 16:06:36 +0900

    敵のUpdate関数追加

--- application/gameobject/component/action/enemy/bomb/BombMoveComponent.cpp
+++ application/gameobject/component/action/enemy/bomb/BombMoveComponent.cpp
@@ -1,1 +1,12 @@
 #include "BombMoveComponent.h"
+
+GameObjectComponent::BombMoveComponent::BombMoveComponent(Camera* camera)
+	: camera_(camera)
+{
+	
+}
+
+void GameObjectComponent::BombMoveComponent::Update(GameObject* owner)
+{
+
+}
--- application/gameobject/component/action/enemy/bomb/BombMoveComponent.h
+++ application/gameobject/component/action/enemy/bomb/BombMoveComponent.h
@@ -1,4 +1,29 @@
 #pragma once
-class BombMoveComponent
+#include "engine/gameobject/component/base/IActionComponent.h"
+#include "jsonEditor/JsonEditableBase.h"
+
+class Camera;
+
+namespace GameObjectComponent
 {
-};
+
+	class BombMoveComponent : public IActionComponent
+	, public JsonEditableBase
+	{
+	public:
+		// カメラのポインタを受け取るコンストラクタ
+		BombMoveComponent(Camera* camera);
+
+		// 更新処理
+		void Update(GameObject* owner) override;
+
+	private:
+		// カメラのポインタを保持
+		Camera* camera_ = nullptr;
+
+		// 移動速度
+		float moveSpeed_ = 10.0f;
+
+	};
+
+} // namespace GameObjectComponent
