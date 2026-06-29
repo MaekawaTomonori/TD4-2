d11a1b50500e4501e699d1df0c14dbe70c751cca
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2026-06-29 17:02:29 +0900

    途中

--- application/gameobject/component/action/player/PlayerReflectComponent.cpp
+++ application/gameobject/component/action/player/PlayerReflectComponent.cpp
@@ -1,1 +1,30 @@
 #include "PlayerReflectComponent.h"
+#include "engine/gameobject/component/collision/SphereColliderComponent.h"
+#include "application/gameobject/component/action/player/PlayerInputComponent.h"
+#include "application/collision/CollisionLayer.h"
+#include "engine/gameobject/base/GameObject.h"
+
+void GameObjectComponent::PlayerReflectComponent::Update(GameObject* owner)
+{
+	if (!collider_)
+	{
+		// 初回Update時にコライダーコンポーネントのポインタを取得
+		collider_ = owner->GetComponent<SphereColliderComponent>().get();
+	}
+
+	// コライダーコンポーネントが取得できていない場合は処理を中断
+	if (!collider_)
+	{
+		return;
+	}
+
+	// 入力があったらコライダーをアクティブにする
+	if (owner->GetComponent<PlayerInputComponent>()->IsReflecting())
+	{
+		collider_->SetCollisionLayer(CollisionLayer::PlayerBullet);
+	}
+	else
+	{
+		collider_->SetCollisionLayer(CollisionLayer::None);
+	}
+}
--- application/gameobject/component/action/player/PlayerReflectComponent.h
+++ application/gameobject/component/action/player/PlayerReflectComponent.h
@@ -1,4 +1,20 @@
 #pragma once
-class PlayerReflectComponent
+#include "engine/gameobject/component/base/IActionComponent.h"
+#include "jsonEditor/JsonEditableBase.h"
+
+class ICollisionComponent;
+
+namespace GameObjectComponent
 {
-};
+	/// @brief プレイヤーの反射処理を制御するコンポーネント
+	class PlayerReflectComponent : public IActionComponent
+		, public JsonEditableBase
+	{
+	public:
+		void Update(GameObject* owner) override;
+
+	private:
+		// 反射するときに使うコライダーコンポーネントのポインタを保持
+		ICollisionComponent* collider_ = nullptr;
+	};
+} // namespace GameObjectComponent
